220ced5d9dabc6e198bd53b4f34916e9b23b9678
SanhoLee <stlsh0516@gmail.com>
2019-12-06 23:21:18 +0900

    0.1 input/output test

--- intpolation.f90
+++ intpolation.f90
@@ -1,5 +1,17 @@
 PROGRAM INTERPOLATION
 	implicit none
-	
+ 
+  ! parameter declaration
+  integer, parameter :: SIZE = 100
+  real, dimension(SIZE) :: x,y
+  character(100) :: read_file="read_file.txt", write_file=""
+  
+  
+  
+  open(10, file=read_file)
+  read(10,*) x(1), y(1)
+
+  write(*,100) x(1), y(1)
+100 format(2(1x, f5.2))
 
 END PROGRAM INTERPOLATION
